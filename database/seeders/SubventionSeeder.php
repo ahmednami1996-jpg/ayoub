54c81debad6446ab2d0bf67bfa185046b94b5376
ahmednami1996-jpg <ahmed.nami.1996@gmail.com>
2025-12-01 16:06:15 +0100

    Update SubventionSeeder.php

--- database/seeders/SubventionSeeder.php
+++ database/seeders/SubventionSeeder.php
@@ -13,20 +13,6 @@ class SubventionSeeder extends Seeder
      */
     public function run(): void
     {
-         $faker = \Faker\Factory::create();
-
-        for ($i = 1; $i <= 30; $i++) {
-            Subvention::create([
-                'title' => $faker->sentence(3),
-                'organization' => $faker->company,
-                'country' => $faker->country,
-                'city' => $faker->city,
-                'file_name' => $faker->word() . '.pdf',
-                'eligibilities' => $faker->paragraph,
-                'views' => $faker->numberBetween(0, 500),
-                'status' => $faker->boolean(80), // 80% chance of true
-                'url' => $faker->url,
-            ]);
-        }
+       
     }
 }
