8dc42de363aef5f24e5f20884efbfa79a8da4257
ahmednami1996-jpg <ahmed.nami.1996@gmail.com>
2025-12-01 16:05:55 +0100

    Update FormationSeeder.php

--- database/seeders/FormationSeeder.php
+++ database/seeders/FormationSeeder.php
@@ -12,27 +12,6 @@ class FormationSeeder extends Seeder
 {
     public function run()
     {
-        $faker = \Faker\Factory::create();
-
-        // Get all category IDs for assigning to formations
-        $categoryIds = Category::pluck('id')->toArray();
-
-        for ($i = 1; $i <= 20; $i++) {
-            Formation::create([
-                'title' => "dedwed",
-                'category_id' => 1,
-                'provider' => "prov",
-                'mode' => 1,
-                'url' => "x.com",
-                'cost' => 98.7,
-                'like' => 89,
-                'picture' => "no-image.png",
-                'reduction' => 4000,
-                'rate' => 3.5,
-                'duration' => 4 . ' months',
-                'views' => 30000,
-                'status' => 1,
-            ]);
-        }
+        
     }
 }
