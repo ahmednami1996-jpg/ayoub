515cca7030b08f32a67e647d5ad761f676f359b7
ahmednami1996-jpg <ahmed.nami.1996@gmail.com>
2025-12-01 16:11:17 +0100

    Update CategorySeeder.php

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -13,43 +13,6 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-  $categories = [
-            'Technologie',
-            'Santé',
-            'Éducation',
-            'Énergie',
-            'Environnement',
-            'Agriculture',
-            'Alimentation & Boissons',
-            'Mode & Textile',
-            'Art & Culture',
-            'Média & Divertissement',
-            'Tourisme & Hôtellerie',
-            'Immobilier',
-            'Transport & Mobilité',
-            'Sport & Fitness',
-            'Finance & Banque',
-            'Marketing & Publicité',
-            'Commerce Électronique',
-            'Applications & Logiciels',
-            'Intelligence Artificielle',
-            'Robotique',
-            'Biotechnologie',
-            'Social & Communautaire',
-            'Événementiel',
-            'Artisanat & Création',
-            'Design & Architecture',
-            'Jeux & Ludique',
-            'Services aux entreprises',
-            'Énergies renouvelables',
-            'Transport durable',
-            'Recherche & Développement',
-        ];
 
-        foreach ($categories as $category) {
-            Category::create([
-                'name' => $category,
-            ]);
-        }
     }
 }
